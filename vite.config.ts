6e3431d0376d10b726d13c297a13cbf3501b3c13
onshinpei <244221001@qq.com>
2025-05-12 21:07:22 +0800

    feat: rename dir website to docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ds-markdown/',
   build: {
     sourcemap: false, // 或 'inline',
-    outDir: 'website',
+    outDir: 'docs',
   },
 });
