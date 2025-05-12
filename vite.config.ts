655ab6462b49f6a4cad5ec17dc8be3c50fa5d5ec
onshinpei <244221001@qq.com>
2025-05-12 20:53:22 +0800

    feat: add website demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: true, // 或 'inline'
+    sourcemap: false, // 或 'inline',
+    outDir: 'website',
   },
-})
+});
